0ab95bef2b46266320efb6cbfea9d7a042878bcd
winnyhutJR <100447657+winnyhutJR@users.noreply.github.com>
2023-08-30 13:56:19 -0700

    Update ExamplePython.py
    added a jobs list

--- ExamplePython.py
+++ ExamplePython.py
@@ -10,3 +10,8 @@ print(f"x = {x} and y = {y}")
 
 #We could also use an f-string like this:
 print("c = {c} and d = {d}".format(c=1, d=2))
+
+job_list = {'John': 'Doctor', 'jane': 'Engineer', 'Jim': 'Teacher'}
+
+for name, job in job_list.items():
+    print(f"{name} is a {job}")
